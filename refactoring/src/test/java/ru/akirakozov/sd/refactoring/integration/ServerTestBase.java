ad3df2c30a58f1566e86efd553d37ecbee756442
kar-arm <kar-arm@yandex-team.ru>
2022-11-07 01:19:40 +0300

    lab3: add base test class for server integration tests

---
+++ refactoring/src/test/java/ru/akirakozov/sd/refactoring/integration/ServerTestBase.java
@@ -0,0 +1,78 @@
+package ru.akirakozov.sd.refactoring.integration;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import ru.akirakozov.sd.refactoring.config.ConfigProvider;
+import ru.akirakozov.sd.refactoring.config.EndpointConfig;
+import ru.akirakozov.sd.refactoring.servlet.AddProductServlet;
+import ru.akirakozov.sd.refactoring.servlet.GetProductsServlet;
+import ru.akirakozov.sd.refactoring.servlet.QueryServlet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class ServerTestBase {
+    private static final String DATABASE_URL = ConfigProvider.dbConfig().url();
+    private static final String DDL = """
+            CREATE TABLE IF NOT EXISTS PRODUCT
+            (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            NAME           TEXT    NOT NULL,
+            PRICE          INT     NOT NULL);
+            """;
+
+    private static final String TRUNCATE = """
+            DELETE FROM PRODUCT;
+            """;
+
+    protected static final EndpointConfig ENDPOINTS = ConfigProvider.endpointPathConfig();
+
+
+    private Server server;
+
+    @BeforeEach
+    public void runApp() throws Exception {
+        setUpDatabase();
+        server = new Server(ConfigProvider.serverConfig().port());
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new AddProductServlet()), ENDPOINTS.addProduct());
+        context.addServlet(new ServletHolder(new GetProductsServlet()), ENDPOINTS.getProducts());
+        context.addServlet(new ServletHolder(new QueryServlet()), ENDPOINTS.query());
+
+        server.start();
+    }
+
+    @AfterEach
+    public void stopApp() throws Exception {
+        pruneDatabase();
+
+        server.stop();
+        server = null;
+    }
+
+
+    private void setUpDatabase() throws SQLException {
+        executeUpdate(DDL);
+    }
+
+    private void pruneDatabase() throws SQLException {
+        executeUpdate(TRUNCATE);
+    }
+
+    private void executeUpdate(String sqlUpdate) throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(DATABASE_URL);
+                Statement statement = connection.createStatement()
+        ) {
+            statement.executeUpdate(sqlUpdate);
+        }
+    }
+}
